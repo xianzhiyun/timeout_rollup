721d38bebefea62dcbb4b1f637e946364032418f
zhangle <zhangle@vtstar.net>
2020-04-12 08:42:23 +0800

    fix(工具打包): 修复nodemon导致重复循环

--- dist/iconfont.cjs.js
+++ dist/iconfont.cjs.js
@@ -1,20 +1,24 @@
-const request = require('request')
-const compressing = require('compressing')
-const path = require('path')
-const fs = require('fs')
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const request = require('request');
+const compressing = require('compressing');
+const path = require('path');
+const fs = require('fs');
 
 class Iconfont {
     constructor(iconConfig) {
-        this.url = iconConfig.url
-        this.toPath = iconConfig.toPath
-        this.cookie = iconConfig.cookie
+        this.url = iconConfig.url;
+        this.toPath = iconConfig.toPath;
+        this.cookie = iconConfig.cookie;
     }
 
     async handleProcess() {
-        let responseBody = await this.getResponseBody(this.url)
-        await this.copyFile(responseBody, this.toPath)
-        await this.deleteFolderRecursive(path.join(__dirname, './iconfont'))
-        await this.deleteFolderRecursive(path.join(__dirname, './iconfont.zip'))
+        let responseBody = await this.getResponseBody(this.url);
+        await this.copyFile(responseBody, this.toPath);
+        await this.deleteFolderRecursive(path.join(__dirname, './iconfont'));
+        await this.deleteFolderRecursive(path.join(__dirname, './iconfont.zip'));
     }
 
     getResponseBody(_url) {
@@ -31,48 +35,47 @@ class Iconfont {
                 // 禁止跳转，防SSRF
                 followRedirect: false,
                 timeout: 10000
-            }
+            };
             request(options, (error, response, body) => {
                 return resolve(body)
-            })
+            });
         })
     }
 
     async copyFile(body,toPath) {
         fs.writeFileSync(path.join(__dirname,'./iconfont.zip'), body);
-        await compressing.zip.uncompress(path.join(__dirname, './iconfont.zip'), path.join(__dirname, './iconfont'))
-        let files = fs.readdirSync(path.join(__dirname, './iconfont'))
-        let targetFiles = fs.readdirSync(path.join(__dirname, `./iconfont/${files[0]}`))
+        await compressing.zip.uncompress(path.join(__dirname, './iconfont.zip'), path.join(__dirname, './iconfont'));
+        let files = fs.readdirSync(path.join(__dirname, './iconfont'));
+        let targetFiles = fs.readdirSync(path.join(__dirname, `./iconfont/${files[0]}`));
         targetFiles.forEach(function (file) {
             fs.copyFileSync(
                 path.join(__dirname, `./iconfont/${files[0]}/${file}`),
                 `${toPath}/${file}`
-            )
-        })
+            );
+        });
         return Promise.resolve()
     }
 
     deleteFolderRecursive(path) {
-        const _this = this
+        const _this = this;
         if (fs.existsSync(path)) {
             if (fs.statSync(path).isDirectory()) {
-                let files = fs.readdirSync(path)
+                let files = fs.readdirSync(path);
                 files.forEach(function (file) {
-                    let curPath = path + '/' + file
+                    let curPath = path + '/' + file;
                     if (fs.statSync(curPath).isDirectory()) {
-                        _this.deleteFolderRecursive.call(this, curPath)
+                        _this.deleteFolderRecursive.call(this, curPath);
                     } else {
-                        fs.unlinkSync(curPath)
+                        fs.unlinkSync(curPath);
                     }
-                })
-                fs.rmdirSync(path)
+                });
+                fs.rmdirSync(path);
             } else {
-                fs.unlinkSync(path)
+                fs.unlinkSync(path);
             }
         }
     }
 
 }
-export {
-    Iconfont
-}
+
+exports.Iconfont = Iconfont;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { Iconfont } from './iconfont';
 
-console.log(1231231321)
 export {
 	Iconfont
 }
